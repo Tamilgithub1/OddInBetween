41aefebef1398c1d2afcf031b56dd4135ef87836
Tamilgithub1 <31219547+Tamilgithub1@users.noreply.github.com>
2018-08-20 20:07:36 +0530

    Create OddInBetween.java

---
+++ OddInBetween.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class wifi{
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int m=in.nextInt();
+		for(int i=n+1;i<m;i++){
+			if(i%2!=0)
+				System.out.print(i+" ");
+		}
+	}
+}
